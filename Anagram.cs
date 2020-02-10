723bda72f71d4c71d27088a20e952e5638665be6
SD7689 <dewanganshivam89@gmail.com>
2020-02-10 18:56:31 +0530

    [Shivam Logger Work]

---
+++ Anagram.cs
@@ -0,0 +1,52 @@
+﻿namespace ConsoleApp1
+{ 
+using System;
+
+    / <summary>
+    / Anagram class
+    / </summary>
+    public class Anagram
+    {
+        / <summary>
+        / The log
+        / </summary>
+        private static readonly log4net.ILog Log = LogHelper.GetLogger();
+
+        / <summary>
+        / Ana the gram.
+        / </summary>
+        public static void AnaGram()
+        {
+            try
+            {
+                Console.WriteLine("Enter String one:");
+                string s1 = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("Enter String Two");
+                string s2 = Convert.ToString(Console.ReadLine());
+                char[] ch1 = s1.ToLower().ToCharArray();
+                char[] ch2 = s2.ToLower().ToCharArray();
+
+                Array.Sort(ch1);
+                Array.Sort(ch2);
+
+                string str1 = new string(ch1);
+                string str2 = new string(ch2);
+
+                if (str1 == str2)
+                {                  
+                    Console.WriteLine("String are Anagaram");
+                }
+                else
+                {
+                    Log.Error("Two strings are not Same or Equal ");
+                    Console.WriteLine("Strings are Not Anagram");
+                }
+            }
+            catch(Exception)
+            {
+                Log.Error("Strings are not of same length!!");
+                throw;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static readonly ILog log = LogHelper.GetLogger(); //log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+                    Console.WriteLine("Hello world");
+
+            log.Error("This is my Error message");
+            Console.ReadLine();
+        }
+    }
+}
